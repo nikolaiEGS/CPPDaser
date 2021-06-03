3ae0f89ed58a039e5e022afd26d717557550a05f
nikolaid <nikolaid@energizeglobal.com>
2021-06-03 13:04:04 +0400

    update smart pointer

--- week 2/smartPointer/smartPointer/main.cpp
+++ week 2/smartPointer/smartPointer/main.cpp
@@ -4,17 +4,23 @@
 
 
 struct Test {
-
+	Test() = default;
+	Test(int nb) : A(nb){}
 	~Test() {
-		std::cout << "DELETE"<<std::endl;
+		//std::cout << "DELETE -- Test destructor"<<std::endl;
+
 	}
 	void print() {
 		std::cout << "print"<<std::endl;
 		std::cout << std::endl;
 	}
+
+	int A;
 };
 
 int main() {
+
+// -------- unique ptr ----------------------------------------------------------------------------------------
 	std::unique_ptr<Test> ptr(new Test());
 	std::unique_ptr<Test[]> arr(new Test[20]);
 	//std::unique_ptr<Test, void (*)(Test*)> t(new Test, [](Test* tt) {tt->print(); });
@@ -22,15 +28,38 @@ int main() {
 	smartPtr::unique_ptr<Test, std::function<void(Test*)>> test(new Test, [](Test* XX) { std::cout << "pointerFunction called" << std::endl; });
 	test->print();
 	(*test).print();
-	test.reset();
-	smartPtr::unique_ptr<Test, std::function<void(Test*)>> tt(new Test, [](Test* XX) { std::cout << "pointerFunction called" << std::endl; });
+	//test.reset();
+	smartPtr::unique_ptr<Test, std::function<void(Test*)>> tt(new Test, [](Test* XX) { /*delete[] XX;*/ std::cout << "pointerFunction called" << std::endl; });
+	//std::cout << "print member string: "<<tt->A << std::endl;
+	smartPtr::unique_ptr<Test, std::function<void(Test*)>> ttt;
+	//ttt = (std::move(tt));
 	//tt = test;
-	//smartPtr::unique_ptr<Test, std::function<void(Test*)>> cc(test);
-	smartPtr::shared_ptr<Test> xx;
+	
+// -------- shared ptr ----------------------------------------------------------------------------------------	
+	smartPtr::shared_ptr<Test, std::function<void(Test*)>> cc(new Test(999));
+	smartPtr::shared_ptr<Test, std::function<void(Test*)>> zz;
+	smartPtr::shared_ptr<Test, std::function<void(Test*)>> gg(cc);
+	smartPtr::shared_ptr<Test, std::function<void(Test*)>> ff(new Test(1111));
+	smartPtr::shared_ptr<Test, std::function<void(Test*)>> jj(std::move(ff));
+	ff.showCount();
+	//ff = gg;
+	cc.showCount();
+	ff.showCount();
+
+
+	//gg.showCount();
+
+	//smartPtr::shared_ptr<Test, std::function<void(Test*)>> nn(cc);
+	//std::cout << gg->A << std::endl;
+	//gg.showCount();
+
+
+
+	/*smartPtr::shared_ptr<Test> xx;
 	xx->print();
 	smartPtr::shared_ptr<Test> yy(xx);
 	yy->print();
 	smartPtr::shared_ptr<Test> kk;
-	kk = yy;
+	kk = yy;*/
 	return 0;
 }
--- week 2/smartPointer/smartPointer/unique_ptr.h
+++ week 2/smartPointer/smartPointer/unique_ptr.h
@@ -1,36 +1,26 @@
 #pragma once
 #include<functional>
-namespace smartPtr{
 
+namespace smartPtr{
+	/*
 	template <typename T, typename F = std::function<void(T*)>>
 	class smart_ptr {
 		T* ptr;
 		F delete_ptr;
-	public:
 		class shared_ptr;
 		class unique_ptr;
+	public:
+		
 		smart_ptr(T* ptr_ = nullptr, F delete_ptr_ = [](T* ptr_) { delete ptr_; }) : ptr(ptr_), delete_ptr(delete_ptr_){}
 		
-		// copy constructor for shared pointer
-		smart_ptr(shared_ptr& copy_ptr) {
-			ptr = copy_ptr.ptr;
-			delete_ptr = copy_ptr.delete_ptr;
-		}
-
 		~smart_ptr() { delete_ptr(this->ptr); }
 		T* operator->() { return ptr; }
 		T& operator*() { return *ptr; }
 
 		void reset() {
 			ptr = nullptr;
-			delete_ptr = [](T* ptr_) { delete ptr_; };
+			delete_ptr = [](T* ptr) { delete ptr; };
 		}
-
-		void swap_ptr(const unique_ptr& left, const unique_ptr& right) {
-			std::swap(left.ptr, right.ptr);
-			std::swap(left.delet_ptr, right.delete_ptr);
-		}
-
 	};
 
 	template <typename T, typename F = std::function<void(T*)>>
@@ -42,12 +32,15 @@ namespace smartPtr{
 		unique_ptr& operator=(const unique_ptr& copyAss) = delete;
 
 		unique_ptr(unique_ptr&& move_ptr) noexcept {
-			swap_ptr(*this, move_ptr);
+			swap(*this, move_ptr);
 			return *this;
 		}
-
+		void swap(const unique_ptr& left, const unique_ptr& right) {
+			std::swap(left.ptr, right.ptr);
+			std::swap(left.delete_ptr, right.delete_ptr);
+		}
 		unique_ptr& operator=(unique_ptr&& move_ptr) noexcept {
-			swap_ptr(*this, move_ptr);
+			swap(*this, move_ptr);
 			return *this;
 		}
 		// constexpr, noexept
@@ -59,16 +52,152 @@ namespace smartPtr{
 	public:
 		shared_ptr(): count_ptr(new int(0)), smart_ptr<T, F>(){}
 		shared_ptr(T* ptr_, F delete_ptr_) : count_ptr(new int(1)), smart_ptr<T, F>(ptr_, delete_ptr_) {}
-		shared_ptr(const shared_ptr& copy_ptr) : smart_ptr<T,F>(copy_ptr){
-			count_ptr++;
+
+		shared_ptr(const shared_ptr& copy_ptr) {
+			smart_ptr<T, F>(copy_ptr.ptr, copy_ptr.delete_ptr);
+			if (copy_ptr.count_ptr != nullptr) {
+				copy_ptr.count_ptr++;
+				count_ptr = copy_ptr.count_ptr;
+			}
+		}
+		void swap(const shared_ptr& left, const shared_ptr& right) {
+			std::swap(left.ptr, right.ptr);
+			std::swap(left.delete_ptr, right.delete_ptr);
 		}
 
+		
+
+		/*
 		shared_ptr& operator=(const shared_ptr assign) {
+			if (this->count_ptr == 1) {
+				//reset();
+			}
+			swap(*this, assign);
+			return *this;
+		}*/
+		/*
+		shared_ptr& operator=(shared_ptr&& assign) {
 			// case 1: if this->count is 0
 			// case 2: if this-> count is 1
 			// case 3: if this-> count is larger then 2
 			swap_ptr(*this, assign);
+			return *this;
 		}
 
+	};*/
+
+	template <typename T, typename F = std::function<void(T*)>>
+	class unique_ptr {
+		T* ptr;
+		F delete_ptr;
+
+	public:
+		unique_ptr(T* ptr_ = nullptr, F delete_ptr_ = [](T* ptr_) { delete ptr_; }) : ptr(ptr_), delete_ptr(delete_ptr_) {}
+		~unique_ptr() { 
+			std::cout << "insight unique destructor " << std::endl;
+			delete_ptr(this->ptr);
+		}
+		unique_ptr(const unique_ptr& copy) = delete;
+		unique_ptr& operator=(const unique_ptr& copyAss) = delete;
+
+		// ----------------- does not work, can not finde the misstake 
+		unique_ptr(unique_ptr&& move_ptr) noexcept {
+			std::cout << "insight move constructor " << std::endl;
+			swap(*this, move_ptr);
+		}
+		// ----------------- does not work ---------------------------------- 
+		unique_ptr& operator=(unique_ptr&& move_ptr) noexcept {
+			std::cout << "insight move assignement " << std::endl;
+			unique_ptr<T,F> tmp(move_ptr);//move ctor
+			swap(*this, tmp);
+			return *this;
+		}
+
+		constexpr void swap(unique_ptr& left, unique_ptr& right) {
+			std::swap(left.ptr, right.ptr);
+			std::swap(left.delete_ptr, right.delete_ptr);
+		}
+	
+		T* operator->() { return ptr; }
+		T& operator*() { return *ptr; }
+
+		void reset(T* t = nullptr) {
+			delete_ptr(this->ptr);
+			ptr = t;
+		}
+	};
+
+
+	template <typename T, typename F = std::function<void(T*)>>
+	class shared_ptr {
+		T* ptr;
+		F delete_ptr;
+		std::size_t* count_ptr;
+	public:
+		// ----------- works -------------------------------------------------
+		shared_ptr(T* ptr_ = nullptr, F delete_ptr_ = [](T* ptr_) { delete ptr_; }) :
+			 ptr(ptr_), delete_ptr(delete_ptr_) {
+			if (ptr) {
+				count_ptr = new std::size_t(1);
+			}
+			else {
+				count_ptr = new std::size_t(0);
+			}
+		}
+		//---------- works ----------------------------
+		~shared_ptr() noexcept { 
+			if (*count_ptr == 1 || *count_ptr == 0) { delete_ptr(this->ptr); }
+			else {*count_ptr -= 1;}
+		}
+		// ------- works -----------------------------------
+		shared_ptr(shared_ptr& copy) {
+			std::cout << "------------ COPY CONSTRUCTOR ----------- " << std::endl;
+			ptr = copy.ptr;
+			delete_ptr = copy.delete_ptr;
+			count_ptr = copy.count_ptr;
+			if (ptr) {*count_ptr += 1;}
+		}
+		// ------------ fails ---------------------------
+		shared_ptr(shared_ptr&& move_ptr) noexcept {
+			if (*move_ptr.count_ptr == 1 || *move_ptr.count_ptr == 0) {
+				swap(*this, move_ptr);
+			}
+		}
+
+		void showCount() {
+			std::cout << "count_ptr == " << *count_ptr << std::endl;
+		}
+		// ------------ fails ---------------------------
+		shared_ptr& operator=(shared_ptr& assign) {
+			std::cout << "------------ insight operator = ----------- "  << std::endl;
+			shared_ptr<T,F> tmp(assign);
+			if (*count_ptr > 1) {
+				std::cout << "------------ insight if ----------- " << std::endl;
+				*count_ptr -= 1;
+				ptr = tmp.ptr;
+				delete_ptr = tmp.delete_ptr;
+				count_ptr = tmp.count_ptr;
+			}
+			else if (*count_ptr == 1 || *count_ptr == 0) {
+				swap(*this, tmp);
+			}
+			return *this;
+		}
+		// ------------ fails ---------------------------
+		shared_ptr& operator=(shared_ptr&& move_ptr) {
+			if (*move_ptr.count_ptr == 1 || *move_ptr.count_ptr == 0) {
+				unique_ptr<T, F> tmp(move_ptr);
+				swap(*this, move_ptr);
+			}
+		}
+		
+		void swap(shared_ptr& left, shared_ptr& right) {
+			std::swap(left.ptr, right.ptr);
+			std::swap(left.delete_ptr, right.delete_ptr);
+			std::swap(left.count_ptr, right.count_ptr);
+		}
+
+		T* operator->() { return ptr; }
+		T& operator*() { return *ptr; }
 	};
 }
