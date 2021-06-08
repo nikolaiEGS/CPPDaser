09c2757360e7f73f4206445c10925afbe7af3757
david <david.tsaturyan.95@gmail.com>
2021-06-08 21:20:12 +0400

    added TODOs

--- lesson 3/Vtable/vtable.h
+++ lesson 3/Vtable/vtable.h
@@ -3,32 +3,30 @@
 #include <functional>
 #include <map>
 
-enum FunctionType {VIRTUAL, NON_VIRTUAL};
+enum FunctionType { VIRTUAL, NON_VIRTUAL };
 
 class Employee {
-	
+
 protected:
 	std::string name;
 	std::size_t age;
 
-	using fpType = std::string(Employee::*)();
-	std::map<std::string, fpType>* vtable;
+	T* vtable; //TODO - You should find the type T by yourself
 
 	std::string info_() { return name + ',' + std::to_string(age); }
 	std::string name_() { return name; }
 public:
 
-	Employee(std::string name_ = "Employee Name", std::size_t age_ = 20) : name(name_), age(age_), vtable (new std::map<std::string, fpType>) {
-		(*vtable)["v_get_info"] = &Employee::info_;
-		(*vtable)["v_get_name"] = &Employee::name_;
-	}
-	
-	virtual ~Employee() {
-		delete vtable;
-	}
-	
+	Employee(std::string name_ = "Employee Name", std::size_t age_ = 20) : name(name_), age(age_), vtable(new T ) {
 
-	virtual std::string get_info() { 
+		//TODO - add a function to the vtable (hint: to know exactly which function you should add look at the virtual std::string get_info())
+		//TODO - add a function to the vtable (hint: to know exactly which function you should add look at the virtual std::string get_name())
+	}
+
+//TODO - implement destructor
+
+
+	virtual std::string get_info() {
 		return info_();
 	}
 
@@ -46,8 +44,7 @@ public:
 
 	std::string v_get_info(FunctionType type) {
 		if (type == FunctionType::VIRTUAL) {
-			const auto& function = vtable->find("v_get_info")->second;
-			return ((*this).*function)();
+			// TODO - call the appropriate function from the vtable 
 		}
 		else {
 			return info_();
@@ -56,8 +53,7 @@ public:
 
 	std::string v_get_name(FunctionType type) {
 		if (type == FunctionType::VIRTUAL) {
-			const auto& function = vtable->find("v_get_name")->second;
-			return ((*this).*function)();
+			// TODO - call the appropriate function from the vtable 
 		}
 		else {
 			return name_();
@@ -74,7 +70,7 @@ public:
 	using Employee::name_;
 
 	Manager(std::string name_ = "Manager Name", std::size_t age_ = 25, std::string level_ = "top") : Employee(name_, age_), level(level_) {
-		(*vtable)["v_get_info"] = reinterpret_cast<std::string(Employee::*)()>(&Manager::info_); // override
+		//TODO - add a function to the vtable (hint: to know exactly which function you should add look at the virtual std::string get_info()) ----- override only get_info, not get_name
 	}
 
 	std::string get_info() override {
